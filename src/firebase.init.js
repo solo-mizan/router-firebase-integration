9f70ebed716bc0d7179ea68aa56fd00b698bead1
solo-mizan <mizanbd.eee@gmail.com>
2022-04-09 23:59:12 +0600

    router components created & firebase auth added

---
+++ src/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBhGQeN3AKkve7DYjWaPqrqtSiuX3FbCzM",
+  authDomain: "router-firebase-integrat-8db13.firebaseapp.com",
+  projectId: "router-firebase-integrat-8db13",
+  storageBucket: "router-firebase-integrat-8db13.appspot.com",
+  messagingSenderId: "1083498189324",
+  appId: "1:1083498189324:web:fc68b3f0d3283c0223d429"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
