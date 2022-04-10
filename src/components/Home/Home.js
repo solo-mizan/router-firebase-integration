389e908dff1c5cdf6a245bdbe2ed08195f29928f
solo-mizan <mizanbd.eee@gmail.com>
2022-04-11 00:38:48 +0600

    data sent to homepage using custom hooks

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import useFirebase from '../../hooks/useFirebase';
 import './Home.css';
 
 const Home = () => {
+
+const {user} = useFirebase();
+
     return (
         <div>
             <h1>This is home</h1>
+            <p>Current user is: {user ? user.displayName : 'No body'}</p>
         </div>
     );
 };
